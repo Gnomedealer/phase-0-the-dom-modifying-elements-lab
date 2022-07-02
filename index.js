64cd74c401f49ffb2d5fdd3fc7c05d44e5a225b0
Tommy Nguyen <sixty22tommy@gmail.com>
2022-07-02 15:26:01 -0700

    commit tree 93f58fc13a20557bd10c8073f5e83a1d73d0d450

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+document.getElementById("main").remove()
+// const main = document.getElementById("main")
+
+// main.remove()
+
+// console.log(main)
+
+ const newHeader = document.createElement("h1")
+ newHeader.id = "victory"
+ console.log(newHeader)
+ 
+// main.append(newHeader)
+newHeader.textContent = "YOUR-NAME is the champion"
+document.body.append(newHeader)
